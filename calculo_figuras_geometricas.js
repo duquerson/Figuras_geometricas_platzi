42029c3ca8fa3e27b7ff3f1ab874362447599e22
Duquerson <sotodjgato@hotmail.com>
2021-08-08 19:49:27 -0400

    modificando elementos formulario3

--- calculo_figuras_geometricas.js
+++ calculo_figuras_geometricas.js
@@ -14,20 +14,24 @@ const perimetro_circulo = (radio) => diametro_circulo(radio) * PI
 const area_circulo = (radio) => PI * (radio ** 2)
     //interaccion con html
 function calcular_perimetro_cuadrado() {
-    const input = document.getElementById("input_cuadrado")
-    const value = input.value
-    let perimetro = perimetro_cuadrado(value)
-    alert(perimetro)
+    const input = document.getElementById("input_cuadrado");
+    const value = input.value;
+    let perimetro = perimetro_cuadrado(value);
+    alert(perimetro);
 }
 const calcular_area_cuadrado = () => {
-    const input = document.getElementById('input_cuadrado')
-    const value = input.value
-    let area = area_cuadrado(value)
-    alert(area)
+    const input = document.getElementById('input_cuadrado');
+    const value = input.value;
+    let area = area_cuadrado(value);
+    alert(area);
 }
 const calcular_perimetro_triangulo = () => {
-    const input = document.getElementById('input_triangulo')
-    const value = input.value
-    const perimetro = perimetro_triangulo(value)
+    const LadoA = document.getElementById('input_lado_A')
+    const lado1 = LadoA.value
+    const LadoB = document.getElementById('input_lado_B')
+    const lado2 = LadoB.value
+    const basetriangulo = document.getElementById('input_lado_Base')
+    const base = basetriangulo.value
+    const perimetro = perimetro_triangulo(lado1, lado2, base)
     alert(perimetro)
 }
