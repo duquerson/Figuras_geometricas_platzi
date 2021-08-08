bdbc2076ccdf03dfd0de75155dbbae812a08960d
Duquerson <sotodjgato@hotmail.com>
2021-08-08 17:01:31 -0400

    creancion de js for html1

--- calculo_figuras_geometricas.js
+++ calculo_figuras_geometricas.js
@@ -2,26 +2,20 @@
   const PI = Math.PI
       //cuadrado
   const perimetro_cuadrado = (lado) => lado * 4
-
   const area_cuadrado = (lado) => {
-      return lado ** 2
-  }
-
-  //triangulo
+          return lado ** 2
+      }
+      //triangulo
   const perimetro_triangulo = (lado1, lado2, base) => lado1 + lado2 + base
   const area_triangulo = (base, altura) => (base * altura) / 2
-
-  //circulo
+      //circulo
   const diametro_circulo = (radio) => radio * 2
-
   const perimetro_circulo = (radio) => diametro_circulo(radio) * PI
   const area_circulo = (radio) => PI * (radio ** 2)
-
-  //interaccion con html
+      //interaccion con html
   function calcular_perimetro_cuadrado() {
       const input = document.getElementById("input_cuadrado")
       const value = input.value
-
       const perimetro = perimetro_cuadrado(value)
       alert(perimetro)
   }
